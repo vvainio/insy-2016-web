3601ff9c6c0520da4b8e3ef4ed5393c4bfea2aab
Ville Vainio <vivainio@cs.helsinki.fi>
2016-04-07 23:13:43 +0300

    Add logic for the PIN code input

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -7,8 +7,17 @@
     $scope.foo = 'bar';
   })
 
-  .controller('AuthCtrl', function ($scope) {
-    $scope.foo = 'bar';
+  .controller('AuthCtrl', function ($scope, $state) {
+    $scope.authForm = {};
+    $scope.regex = /^[0-9]{5}$/;
+
+    $scope.didInputPin = function () {
+      if ($scope.authForm.pin === 12345) {
+        $state.go('send');
+      }
+
+      $scope.authForm.pin = undefined;
+    };
   })
 
   .controller('SendCtrl', function () {});
